89a4191ee2c7f783d66bce5c330c9e937041f630
Serghei Iakovlev <serghei@phalconphp.com>
2018-05-11 12:34:09 +0300

    Improved autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -10,6 +10,7 @@ test -n "$srcdir" || srcdir=.
 ORIGDIR=`pwd`
 cd $srcdir
 
+echo "Checking for autoreconf ..."
 (autoreconf --version) < /dev/null > /dev/null 2>&1 || {
   (autoconf --version) < /dev/null > /dev/null 2>&1 || {
     echo
@@ -17,6 +18,7 @@ cd $srcdir
     echo "libtool, etc) to update the build system.  Download the appropriate"
     echo "packages for your distribution, or get the source tar balls from"
     echo "ftp://ftp.gnu.org/pub/gnu/."
+    echo
     exit 1
   }
   echo
@@ -24,18 +26,34 @@ cd $srcdir
   echo "to update the build system.  Download the appropriate updated package"
   echo "for your distribution, or get the source tar ball from"
   echo "ftp://ftp.gnu.org/pub/gnu/."
+  echo
   exit 1
 }
 
-echo "Running autoreconf --verbose --install --force --warnings=all"
+echo "Checking for aclocal ..."
+(aclocal --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "**Error**: You must have GNU Automake to compile this package."
+    echo "Download the appropriate package for your distribution,"
+    echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+    echo
+    exit 1
+}
+
+echo "Start autotools ..."
+
+echo "Running autoreconf ..."
 # - The --force option rebuilds the configure script regardless of its
 #   timestamp in relation to that of the file configure.ac.
 # - The option --install copies some missing files to the directory,
 #   including the text files e.g. COPYING and INSTALL.
 autoreconf --verbose --install --force --warnings=all || exit $?
 
-echo "Removing autom4te.cache"
+echo "Removing autom4te.cache ..."
 rm -rf autom4te.cache
 
 cd $ORIGDIR
-test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+test -n "$NOCONFIGURE" || ( echo "Running ./configure with $@" && "$srcdir/configure" "$@" )
+
+echo "Finished."
+echo
